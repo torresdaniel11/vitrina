3950d0bcf428582a8b296790bf89ba6cd638fe7a
Daniel <d.torres11@uniandes.edu.co>
2018-09-02 14:57:45 -0500

    cars data *not completed

---
+++ src/app/_data/car-data.ts
@@ -0,0 +1,72 @@
+export const CarData = [
+  {
+    carId:"id1",
+    thumbnail: "assets/images/thumbnail/fiesta.png",
+    brand: "Ford",
+    model: "Fiesta",
+    year: 2001,
+    price: 27000000,
+    characteristics: "",
+    images: [
+      "assets/images/car_model/fiesta/fiesta_1.png",
+      "assets/images/car_model/fiesta/fiesta_2.png",
+      "assets/images/car_model/fiesta/fiesta_3.png",
+      "assets/images/car_model/fiesta/fiesta_4.png",
+      "assets/images/car_model/fiesta/fiesta_5.png",
+      "assets/images/car_model/fiesta/fiesta_6.png",
+      "assets/images/car_model/fiesta/fiesta_7.png"
+    ]
+  },
+  {
+    carId:"id2",
+    thumbnail: "assets/images/thumbnail/cruze.png",
+    brand: "Chevrolet",
+    model: "Cruze",
+    year: 2008,
+    price: 30900100,
+    characteristics: "",
+    images: [
+      "assets/images/car_model/cruze/cruze_1.png",
+      "assets/images/car_model/cruze/cruze_2.png",
+      "assets/images/car_model/cruze/cruze_3.png",
+      "assets/images/car_model/cruze/cruze_4.png",
+      "assets/images/car_model/cruze/cruze_5.png",
+      "assets/images/car_model/cruze/cruze_6.png"
+    ]
+  },
+  {
+    carId:"id3",
+    thumbnail: "assets/images/thumbnail/fiesta.png",
+    brand: "Ford",
+    model: "Fiesta",
+    year: 2010,
+    price: 27000000,
+    characteristics: "",
+    images: [
+      "assets/images/car_model/fiesta/fiesta_1.png",
+      "assets/images/car_model/fiesta/fiesta_2.png",
+      "assets/images/car_model/fiesta/fiesta_3.png",
+      "assets/images/car_model/fiesta/fiesta_4.png",
+      "assets/images/car_model/fiesta/fiesta_5.png",
+      "assets/images/car_model/fiesta/fiesta_6.png",
+      "assets/images/car_model/fiesta/fiesta_7.png"
+    ]
+  },
+  {
+    carId:"id4",
+    thumbnail: "assets/images/thumbnail/cruze.png",
+    brand: "Chevrolet",
+    model: "Cruze",
+    year: 2017,
+    price: 30900100,
+    characteristics: "",
+    images: [
+      "assets/images/car_model/cruze/cruze_1.png",
+      "assets/images/car_model/cruze/cruze_2.png",
+      "assets/images/car_model/cruze/cruze_3.png",
+      "assets/images/car_model/cruze/cruze_4.png",
+      "assets/images/car_model/cruze/cruze_5.png",
+      "assets/images/car_model/cruze/cruze_6.png"
+    ]
+  }
+];
