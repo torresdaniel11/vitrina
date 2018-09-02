ea0ff2b745568b42b2086ee9da832d8232314c23
Daniel <d.torres11@uniandes.edu.co>
2018-09-02 14:57:53 -0500

    car interface

---
+++ src/app/_models/car.model.ts
@@ -0,0 +1,10 @@
+export interface Car {
+  carId: string,
+  thumbnail: string,
+  model: string,
+  year: number,
+  brand: string,
+  price: number,
+  characteristics: string,
+  images: string[]
+}
