aa8d6b2cc56b4351c4b0351455e8ff39a3d9944a
Daniel <d.torres11@uniandes.edu.co>
2018-09-02 23:23:24 -0500

    comparison component

--- src/app/components/comparison/comparison.component.ts
+++ src/app/components/comparison/comparison.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Car } from '../../_models/car.model';
 
 @Component({
   selector: 'app-comparison',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComparisonComponent implements OnInit {
 
+  @Input("carsToCompare")
+  carsToCompare: Car[]
   constructor() { }
 
   ngOnInit() {
