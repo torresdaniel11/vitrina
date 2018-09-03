f17a07e37c8ac34af69c4f6bd2d3b38df7342360
Daniel <d.torres11@uniandes.edu.co>
2018-09-02 23:23:12 -0500

    more cars data

--- src/app/_data/car-data.ts
+++ src/app/_data/car-data.ts
@@ -1,6 +1,6 @@
 export const CarData = [
   {
-    carId: "id1",
+    carId: "id91",
     thumbnail: "assets/images/thumbnail/fiesta.png",
     brand: "Ford",
     model: "Fiesta",
@@ -31,70 +31,7 @@ export const CarData = [
     ]
   },
   {
-    carId: "id2",
-    thumbnail: "assets/images/thumbnail/cruze.png",
-    brand: "Chevrolet",
-    model: "Cruze",
-    year: 2008,
-    price: 30900100,
-    characteristics: "",
-    tecnical: {
-      color: "Blanco",
-      new: true,
-      version: "HATCHBACK SE MT 1600CC",
-      direction: "Hydraulic",
-      engine: 1597,
-      transmission: "Mechanic",
-      traction: "4x2"
-    },
-    security: {
-      absbrake: true,
-      airbag: true,
-      alarm: true
-    },
-    images: [
-      "assets/images/car_model/cruze/cruze_1.png",
-      "assets/images/car_model/cruze/cruze_2.png",
-      "assets/images/car_model/cruze/cruze_3.png",
-      "assets/images/car_model/cruze/cruze_4.png",
-      "assets/images/car_model/cruze/cruze_5.png",
-      "assets/images/car_model/cruze/cruze_6.png"
-    ]
-  },
-  {
-    carId: "id3",
-    thumbnail: "assets/images/thumbnail/fiesta.png",
-    brand: "Ford",
-    model: "Fiesta",
-    year: 2010,
-    price: 27000000,
-    characteristics: "",
-    tecnical: {
-      color: "Blanco",
-      new: true,
-      version: "HATCHBACK SE MT 1600CC",
-      direction: "Hydraulic",
-      engine: 1597,
-      transmission: "Mechanic",
-      traction: "4x2"
-    },
-    security: {
-      absbrake: true,
-      airbag: true,
-      alarm: true
-    },
-    images: [
-      "assets/images/car_model/fiesta/fiesta_1.png",
-      "assets/images/car_model/fiesta/fiesta_2.png",
-      "assets/images/car_model/fiesta/fiesta_3.png",
-      "assets/images/car_model/fiesta/fiesta_4.png",
-      "assets/images/car_model/fiesta/fiesta_5.png",
-      "assets/images/car_model/fiesta/fiesta_6.png",
-      "assets/images/car_model/fiesta/fiesta_7.png"
-    ]
-  },
-  {
-    carId: "id4",
+    carId: "id94",
     thumbnail: "assets/images/thumbnail/cruze.png",
     brand: "Chevrolet",
     model: "Cruze",
@@ -123,5 +60,577 @@ export const CarData = [
       "assets/images/car_model/cruze/cruze_5.png",
       "assets/images/car_model/cruze/cruze_6.png"
     ]
+  },
+  {
+    carId: "id1",
+    thumbnail: "assets/images/thumbnail/A4.jpg",
+    brand: "Audi",
+    model: "A4",
+    year: 2008,
+    price: 30900000,
+    details: {
+      color: "Rojo",
+      new: false,
+      version: "B7 1.8T AVANT LUXURY TP 1800CC T",
+      direction: "Hidráulica",
+      engine: '1800 Turbo',
+      transmission: "Hidráulica",
+      traction: "4x2"
+    },
+    security: {
+      absbrake: true,
+      airbag: true,
+      alarm: true
+    },
+    images: [
+      "assets/images/car_model/A4/A4_1.PNG",
+      "assets/images/car_model/A4/A4_2.PNG",
+      "assets/images/car_model/A4/A4_3.PNG",
+      "assets/images/car_model/A4/A4_4.PNG",
+      "assets/images/car_model/A4/A4_5.PNG",
+      "assets/images/car_model/A4/A4_6.PNG",
+      "assets/images/car_model/A4/A4_7.PNG"
+    ]
+  },
+  {
+
+    carId: "id2",
+    thumbnail: "assets/images/thumbnail/Q5.jpg",
+    brand: "Audi",
+    model: "Q5",
+    year: 2010,
+    price: 55000000,
+    details: {
+      color: "Plateado",
+      new: false,
+      version: "2.0 TDI ATTRACTION MT 2000CC TD",
+      direction: "Hidráulica",
+      engine: "TURBO DIESEL 2.0",
+      transmission: "Mecánica",
+      traction: "4x4"
+    },
+    security: {
+      absbrake: true,
+      airbag: true,
+      alarm: true
+    },
+    images: [
+      "assets/images/car_model/Q5/Q5_1.PNG",
+      "assets/images/car_model/Q5/Q5_2.PNG",
+      "assets/images/car_model/Q5/Q5_3.PNG",
+      "assets/images/car_model/Q5/Q5_4.PNG",
+      "assets/images/car_model/Q5/Q5_5.PNG",
+      "assets/images/car_model/Q5/Q5_6.PNG",
+    ]
+  },
+  {
+    carId: "id3",
+    thumbnail: "assets/images/thumbnail/TT.jpg",
+    brand: "Audi",
+    model: "TT",
+    year: 2017,
+    price: 137800000,
+    details: {
+      color: "Azul",
+      new: false,
+      version: "S COUPE 2.0 TFSI QUATTRO 265 HP S TRONIC",
+      direction: "Hidráulica",
+      engine: 2000,
+      transmission: "Automática",
+      traction: "4x2"
+    },
+    security: {
+      absbrake: true,
+      airbag: true,
+      alarm: true
+    },
+    images: [
+      "assets/images/car_model/TT/TT_1.PNG",
+      "assets/images/car_model/TT/TT_2.PNG",
+      "assets/images/car_model/TT/TT_3.PNG",
+      "assets/images/car_model/TT/TT_4.PNG",
+      "assets/images/car_model/TT/TT_5.PNG",
+      "assets/images/car_model/TT/TT_6.PNG",
+    ]
+  },
+  {
+    carId: "id4",
+    thumbnail: "assets/images/thumbnail/Q7.jpg",
+    brand: "Audi",
+    model: "Q7",
+    year: 2011,
+    price: 113000000,
+    details: {
+      color: "Gris",
+      new: false,
+      version: "V6 3.6 FSI QUATTRO TP 3600CC",
+      direction: "Asistida",
+      engine: 3600,
+      transmission: "Automática",
+      traction: "4x4",
+    },
+    security: {
+      absbrake: true,
+      airbag: true,
+      alarm: true
+    },
+    images: [
+      "assets/images/car_model/Q7/Q7_1.PNG",
+      "assets/images/car_model/Q7/Q7_2.PNG",
+      "assets/images/car_model/Q7/Q7_3.PNG",
+      "assets/images/car_model/Q7/Q7_4.PNG",
+      "assets/images/car_model/Q7/Q7_5.PNG",
+      "assets/images/car_model/Q7/Q7_6.PNG",
+    ]
+  },
+  {
+    carId: "id5",
+    thumbnail: "assets/images/thumbnail/Escape.jpg",
+    brand: "Ford",
+    model: "Escape",
+    year: 2012,
+    price: 44900000,
+    details: {
+      color: "Rojo	",
+      new: false,
+      version: "XLS AT 3000CC 4X4",
+      direction: "Hidráulica",
+      engine: 3000,
+      transmission: "Automática",
+      traction: "4x4"
+    },
+    security: {
+      absbrake: true,
+      airbag: true,
+      alarm: true
+    },
+    images: [
+      "assets/images/car_model/Escape/Escape_1.PNG",
+      "assets/images/car_model/Escape/Escape_2.PNG",
+      "assets/images/car_model/Escape/Escape_3.PNG",
+      "assets/images/car_model/Escape/Escape_4.PNG",
+      "assets/images/car_model/Escape/Escape_5.PNG",
+      "assets/images/car_model/Escape/Escape_6.PNG",
+    ]
+  },
+  {
+    carId: "id6",
+    thumbnail: "assets/images/thumbnail/Focus.jpg",
+    brand: "Ford",
+    model: "Focus",
+    year: 2015,
+    price: 49900000,
+    details: {
+      color: "Azul",
+      new: false,
+      version: "TITANIUM TP 2000CC 4P",
+      direction: "Hidráulica",
+      engine: 2000,
+      transmission: "Automática",
+      traction: "4x2"
+    },
+    security: {
+      absbrake: true,
+      airbag: true,
+      alarm: true
+    },
+    images: [
+      "assets/images/car_model/Focus/Focus_1.PNG",
+      "assets/images/car_model/Focus/Focus_2.PNG",
+      "assets/images/car_model/Focus/Focus_3.PNG",
+      "assets/images/car_model/Focus/Focus_4.PNG",
+      "assets/images/car_model/Focus/Focus_5.PNG",
+      "assets/images/car_model/Focus/Focus_6.PNG",
+    ]
+  },
+  {
+    carId: "id7",
+    thumbnail: "assets/images/thumbnail/EcoSport.jpg",
+    brand: "Ford",
+    model: "EcoSport",
+    year: 2008,
+    price: 28800000,
+    details: {
+      color: "Negro",
+      new: false,
+      version: "2.0 L MT 2000CC 4X4",
+      direction: "Hidráulica",
+      engine: 2000,
+      transmission: "Mecánica",
+      traction: "4x4"
+    },
+    security: {
+      absbrake: true,
+      airbag: true,
+      alarm: true
+    },
+    images: [
+      "assets/images/car_model/EcoSport/EcoSport_1.PNG",
+      "assets/images/car_model/EcoSport/EcoSport_2.PNG",
+      "assets/images/car_model/EcoSport/EcoSport_3.PNG",
+      "assets/images/car_model/EcoSport/EcoSport_4.PNG",
+      "assets/images/car_model/EcoSport/EcoSport_5.PNG",
+      "assets/images/car_model/EcoSport/EcoSport_6.PNG",
+    ]
+  },
+  {
+    carId: "id8",
+    thumbnail: "assets/images/thumbnail/Explorer.jpg",
+    brand: "Ford",
+    model: "Explorer",
+    year: 2014,
+    price: 82500000,
+    details: {
+      color: "Rojo",
+      new: false,
+      version: "LIMITED AT 3500CC 4X4",
+      direction: "Hidráulica",
+      engine: 4000,
+      transmission: "Automática",
+      traction: "4x4"
+    },
+    security: {
+      absbrake: true,
+      airbag: true,
+      alarm: true
+    },
+    images: [
+      "assets/images/car_model/Explorer/Explorer_1.PNG",
+      "assets/images/car_model/Explorer/Explorer_2.PNG",
+      "assets/images/car_model/Explorer/Explorer_3.PNG",
+      "assets/images/car_model/Explorer/Explorer_4.PNG",
+      "assets/images/car_model/Explorer/Explorer_5.PNG",
+      "assets/images/car_model/Explorer/Explorer_6.PNG",
+    ]
+  },
+  {
+    carId: "id9",
+    thumbnail: "assets/images/thumbnail/Captiva.jpg",
+    brand: "Chevrolet",
+    model: "Captiva",
+    year: 2010,
+    price: 28750000,
+    details: {
+      color: "Negro",
+      new: false,
+      version: "SPORT AT 3600CC 5P",
+      direction: "Hidráulica",
+      engine: 3600,
+      transmission: "Automática",
+      traction: "4x4"
+    },
+    security: {
+      absbrake: true,
+      airbag: true,
+      alarm: true
+    },
+    images: [
+      "assets/images/car_model/Captiva/Captiva_1.PNG",
+      "assets/images/car_model/Captiva/Captiva_2.PNG",
+      "assets/images/car_model/Captiva/Captiva_3.PNG",
+      "assets/images/car_model/Captiva/Captiva_4.PNG",
+      "assets/images/car_model/Captiva/Captiva_5.PNG",
+      "assets/images/car_model/Captiva/Captiva_6.PNG",
+    ]
+  },
+  {
+    carId: "id10",
+    thumbnail: "assets/images/thumbnail/Camaro.jpg",
+    brand: "Chevrolet",
+    model: "Camaro",
+    year: 2017,
+    price: 172990000,
+    details: {
+      color: "Azul",
+      new: true,
+      version: "SS AT 6200CC",
+      direction: "Asistida",
+      engine: 6200,
+      transmission: "Automática",
+      traction: "4x2"
+    },
+    security: {
+      absbrake: true,
+      airbag: true,
+      alarm: true
+    },
+    images: [
+      "assets/images/car_model/Camaro/Camaro_1.PNG",
+      "assets/images/car_model/Camaro/Camaro_2.PNG",
+      "assets/images/car_model/Camaro/Camaro_3.PNG",
+      "assets/images/car_model/Camaro/Camaro_4.PNG",
+      "assets/images/car_model/Camaro/Camaro_5.PNG",
+      "assets/images/car_model/Camaro/Camaro_6.PNG",
+    ]
+  },
+  {
+    carId: "id11",
+    thumbnail: "assets/images/thumbnail/Tracker.jpg",
+    brand: "Chevrolet",
+    model: "Tracker",
+    year: 2016,
+    price: 43900000,
+    details: {
+      color: "Blanco",
+      new: false,
+      version: "LS MT 1800CC",
+      direction: "Hidráulica",
+      engine: 1800,
+      transmission: "Mecánica",
+      traction: "4x2"
+    },
+    security: {
+      absbrake: true,
+      airbag: true,
+      alarm: true
+    },
+    images: [
+      "assets/images/car_model/Tracker/Tracker_1.PNG",
+      "assets/images/car_model/Tracker/Tracker_2.PNG",
+      "assets/images/car_model/Tracker/Tracker_3.PNG",
+      "assets/images/car_model/Tracker/Tracker_4.PNG",
+      "assets/images/car_model/Tracker/Tracker_5.PNG",
+      "assets/images/car_model/Tracker/Tracker_6.PNG",
+    ]
+  },
+  {
+    carId: "id12",
+    thumbnail: "assets/images/thumbnail/M235.jpg",
+    brand: "BMW",
+    model: "M235",
+    year: 2015,
+    price: 105000000,
+    details: {
+      color: "Blanco",
+      new: false,
+      version: "Coupe TP 3000 CC T",
+      direction: "Hidráulica",
+      engine: 3000,
+      transmission: "Automática",
+      traction: "4x2"
+    },
+    security: {
+      absbrake: true,
+      airbag: true,
+      alarm: true
+    },
+    images: [
+      "assets/images/car_model/M235/M235_1.PNG",
+      "assets/images/car_model/M235/M235_2.PNG",
+      "assets/images/car_model/M235/M235_3.PNG",
+      "assets/images/car_model/M235/M235_4.PNG",
+      "assets/images/car_model/M235/M235_5.PNG",
+      "assets/images/car_model/M235/M235_6.PNG",
+    ]
+  },
+  {
+    carId: "id13",
+    thumbnail: "assets/images/thumbnail/Serie 1.jpg",
+    brand: "BMW",
+    model: "Serie 1",
+    year: 2015,
+    price: 68500000,
+    details: {
+      color: "Gris",
+      new: false,
+      version: "118i PAQUETE M",
+      direction: "Asistida",
+      engine: 1600,
+      transmission: "Automática",
+      traction: "4x2"
+    },
+    security: {
+      absbrake: true,
+      airbag: true,
+      alarm: true
+    },
+    images: [
+      "assets/images/car_model/Serie 1/Serie 1_1.PNG",
+      "assets/images/car_model/Serie 1/Serie 1_2.PNG",
+      "assets/images/car_model/Serie 1/Serie 1_3.PNG",
+      "assets/images/car_model/Serie 1/Serie 1_4.PNG",
+      "assets/images/car_model/Serie 1/Serie 1_5.PNG",
+      "assets/images/car_model/Serie 1/Serie 1_6.PNG",
+    ]
+  },
+  {
+    carId: "id14",
+    thumbnail: "assets/images/thumbnail/X5.jpg",
+    brand: "BMW",
+    model: "X5",
+    year: 2015,
+    price: 195000000,
+    details: {
+      color: "Gris",
+      new: false,
+      version: "[E70] xDrive30d Premium TP 3000CC TD",
+      direction: "Asistida",
+      engine: 3000,
+      transmission: "Automática",
+      traction: "4x4"
+    },
+    security: {
+      absbrake: true,
+      airbag: true,
+      alarm: true
+    },
+    images: [
+      "assets/images/car_model/X5/X5_1.PNG",
+      "assets/images/car_model/X5/X5_2.PNG",
+      "assets/images/car_model/X5/X5_3.PNG",
+      "assets/images/car_model/X5/X5_4.PNG",
+      "assets/images/car_model/X5/X5_5.PNG",
+    ]
+  },
+  {
+    carId: "id15",
+    thumbnail: "assets/images/thumbnail/Z4.jpg",
+    brand: "BMW",
+    model: "Z4",
+    year: 2015,
+    price: 98000000,
+    details: {
+      color: "Negro",
+      new: false,
+      version: "E89 SDRIVE20I TP 2000CC",
+      direction: "Asistida",
+      engine: 2000,
+      transmission: "Automática",
+      traction: "4x2"
+    },
+    security: {
+      absbrake: true,
+      airbag: true,
+      alarm: true
+    },
+    images: [
+      "assets/images/car_model/Z4/Z4_1.PNG",
+      "assets/images/car_model/Z4/Z4_2.PNG",
+      "assets/images/car_model/Z4/Z4_3.PNG",
+      "assets/images/car_model/Z4/Z4_4.PNG",
+      "assets/images/car_model/Z4/Z4_5.PNG",
+      "assets/images/car_model/Z4/Z4_6.PNG",
+    ]
+  },
+  {
+    carId: "id16",
+    thumbnail: "assets/images/thumbnail/Cherokee.jpg",
+    brand: "Jeep",
+    model: "Cherokee",
+    year: 2014,
+    price: 66000000,
+    details: {
+      color: "Beige",
+      new: false,
+      version: "LONGITUDE AT 3200CC",
+      direction: "Hidráulica",
+      engine: 3200,
+      transmission: "Automática",
+      traction: "4x4"
+    },
+    security: {
+      absbrake: true,
+      airbag: true,
+      alarm: true
+    },
+    images: [
+      "assets/images/car_model/Cherokee/Cherokee_1.PNG",
+      "assets/images/car_model/Cherokee/Cherokee_2.PNG",
+      "assets/images/car_model/Cherokee/Cherokee_3.PNG",
+      "assets/images/car_model/Cherokee/Cherokee_4.PNG",
+      "assets/images/car_model/Cherokee/Cherokee_5.PNG",
+      "assets/images/car_model/Cherokee/Cherokee_6.PNG",
+    ]
+  },
+  {
+    carId: "id17",
+    thumbnail: "assets/images/thumbnail/Wrangler.jpg",
+    brand: "Jeep",
+    model: "Wrangler",
+    year: 2016,
+    price: 107000000,
+    details: {
+      color: "Azul",
+      new: false,
+      version: "SPORT AT 3600CC",
+      direction: "Hidráulica",
+      engine: 3600,
+      transmission: "Automática",
+      traction: "4x4"
+    },
+    security: {
+      absbrake: true,
+      airbag: true,
+      alarm: true
+    },
+    images: [
+      "assets/images/car_model/Wrangler/Wrangler_1.PNG",
+      "assets/images/car_model/Wrangler/Wrangler_2.PNG",
+      "assets/images/car_model/Wrangler/Wrangler_3.PNG",
+      "assets/images/car_model/Wrangler/Wrangler_4.PNG",
+      "assets/images/car_model/Wrangler/Wrangler_5.PNG",
+      "assets/images/car_model/Wrangler/Wrangler_6.PNG",
+    ]
+  },
+  {
+    carId: "id18",
+    thumbnail: "assets/images/thumbnail/Gran Cherokee.jpg",
+    brand: "Jeep",
+    model: "Gran Cherokee",
+    year: 2012,
+    price: 84500000,
+    details: {
+      color: "Gris",
+      new: false,
+      version: "LIMITED TP 5700CC 4X4",
+      direction: "Hidráulica",
+      engine: "HEMI 5700",
+      transmission: "Automática",
+      traction: "4x4"
+    },
+    security: {
+      absbrake: true,
+      airbag: true,
+      alarm: true
+    },
+    images: [
+      "assets/images/car_model/Gran Cherokee/Gran Cherokee_1.PNG",
+      "assets/images/car_model/Gran Cherokee/Gran Cherokee_2.PNG",
+      "assets/images/car_model/Gran Cherokee/Gran Cherokee_3.PNG",
+      "assets/images/car_model/Gran Cherokee/Gran Cherokee_4.PNG",
+      "assets/images/car_model/Gran Cherokee/Gran Cherokee_5.PNG",
+      "assets/images/car_model/Gran Cherokee/Gran Cherokee_6.PNG",
+    ]
+  },
+  {
+    carId: "id19",
+    thumbnail: "assets/images/thumbnail/Patriot.jpg",
+    brand: "Jeep",
+    model: "Patriot",
+    year: 2007,
+    price: 33890000,
+    details: {
+      color: "Azul",
+      new: false,
+      version: "LIMITED AT 2400CC 4X4 5P",
+      direction: "Hidráulica",
+      engine: 2400,
+      transmission: "Automática",
+      traction: "4x4"
+    },
+    security: {
+      absbrake: true,
+      airbag: true,
+      alarm: true
+    },
+    images: [
+      "assets/images/car_model/Patriot/Patriot_1.PNG",
+      "assets/images/car_model/Patriot/Patriot_2.PNG",
+      "assets/images/car_model/Patriot/Patriot_3.PNG",
+      "assets/images/car_model/Patriot/Patriot_4.PNG",
+      "assets/images/car_model/Patriot/Patriot_5.PNG",
+      "assets/images/car_model/Patriot/Patriot_6.PNG",
+    ]
+
   }
-];
+]
