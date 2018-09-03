6b79b0ad1c3297cf95722cbadcb6571c86dd8ede
Daniel <d.torres11@uniandes.edu.co>
2018-09-02 20:12:21 -0500

    landing add feature to enable select to compare

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -11,18 +11,51 @@ import { CarService } from '../../_services/car.service';
 export class LandingComponent implements OnInit {
 
   carsToShow: Car[];
+  carsToCompare: Car[];
   searchText: string;
+  selectMode: boolean;
   constructor(private cars: CarService) {
+    this.carsToCompare = [];
     this.carsToShow = cars.getCars();
-
-    //async mode
-    // cars.getCarsAsync().then((carData: Car[]) => {
-    //   this.carsToShow = carData
-    // });
+    this.selectMode = false;
   }
 
   ngOnInit() {
-    console.log(this.carsToShow)
+  }
+
+  selectToCompare(car: Car) {
+    if (this.isSelected(car)) {
+      this.removeSelectedCar(car)
+    } else if (this.carsToCompare.length < 3) {
+      this.carsToCompare.push(car);
+    } else {
+      alert("ya tienes 3 escogidos")
+    }
+  }
+
+  /*
+    param: car:Car
+    Remove the car from the carsToCompare Array
+  */
+  removeSelectedCar(car: Car) {
+    this.carsToCompare.splice(this.getArrayPosition(car), 1)
+  }
+
+  /*
+    param: car:Car
+    return true if the car is inside carsToCompare Array
+  */
+  isSelected(car: Car): boolean {
+    return this.getArrayPosition(car) >= 0;
+  }
+
+  /*
+    param: car:Car
+      return the position of car inside carsToCompare Array
+      return -1 if the car is not in the array
+  */
+  getArrayPosition(car: Car): number {
+    return this.carsToCompare.indexOf(car)
   }
 
 
